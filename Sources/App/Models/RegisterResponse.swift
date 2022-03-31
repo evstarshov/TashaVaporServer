e11a8407891e4e0e167059bd91b4a087fa291e92
Crocodil <evstarshov@icloud.com>
2022-03-31 14:41:22 +0300

    Register request, register response

---
+++ Sources/App/Models/RegisterResponse.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Евгений Старшов on 31.03.2022.
+//
+
+import Vapor
+struct RegisterResponse: Content {
+    var result: Int
+    var user_message: String?
+    var error_message: String?
+}
