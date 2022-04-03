4c0323677ef1d6160e1f4a0f7b9ad91be22ee1a4
Crocodil <evstarshov@icloud.com>
2022-04-03 13:23:25 +0300

    +3 user functions

--- Sources/App/Controllers/AuthController.swift
+++ Sources/App/Controllers/AuthController.swift
@@ -43,7 +43,32 @@ class AuthController {
         return req.eventLoop.future(successResponse)
     }
     
+    func auth(_ req: Request) throws -> EventLoopFuture<DefaultResponse> {
+        guard let body = try? req.content.decode(User.self) else { throw Abort(.badRequest) }
+        
+        if let userLogin = body.login, let userPassword = body.password {
+            if userLogin != "rakodill" || userPassword != "mypass" {
+                return req.eventLoop.future(errorWrongCredentials)
+            } else {
+                return req.eventLoop.future(successResponse)
+            }
+        } else {
+            return req.eventLoop.future(errorIncompleteDataResponse)
+        }
+    }
     
+    func logout(_ req: Request) throws -> EventLoopFuture<DefaultResponse> {
+        guard let _ = try? req.content.decode(User.self) else { throw Abort(.badRequest) }
+        return req.eventLoop.future(successResponse)
+    }
     
-
+    func changeUserData(_ req: Request) throws -> EventLoopFuture<DefaultResponse> {
+        guard let body = try? req.content.decode(User.self) else { throw Abort(.badRequest) }
+        
+        guard body.id != nil, body.login != nil, body.password != nil, body.name != nil, body.lastname != nil, body.email != nil else {
+            return req.eventLoop.future(errorIncompleteDataResponse)
+        }
+        
+        return req.eventLoop.future(successResponse)
+    }
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,5 +3,8 @@ import Vapor
 
 func routes(_ app: Application) throws {
     let controller = AuthController()
-        app.post("register", use: controller.register)
+    app.post("auth", use: controller.auth)
+    app.post("logout", use: controller.logout)
+    app.post("register", use: controller.register)
+    app.post("changeuserdata", use: controller.changeUserData)
 }
