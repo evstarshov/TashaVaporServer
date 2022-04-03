b525f03bb07c9593074489324c406b74dff31d11
Crocodil <evstarshov@icloud.com>
2022-04-03 11:25:27 +0300

    review response

---
+++ Sources/App/Models/Review.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Евгений Старшов on 03.04.2022.
+//
+
+import Foundation
+
+struct Review: Content {
+    var productId: Int?
+    var userId: Int?
+    var reviewText: String?
+    var reviewId: Int?
+}
---
+++ Sources/App/Models/ReviewResponse.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Евгений Старшов on 03.04.2022.
+//
+
+import Foundation
+
+struct ReviewResponse: Content {
+    let userId: Int?
+    let reviewText: String?
+}
