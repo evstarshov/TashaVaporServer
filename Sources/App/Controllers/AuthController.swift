a147808a23d33f147897b80323df57fb7174995a
Crocodil <evstarshov@icloud.com>
2022-04-03 11:42:23 +0300

    auth controller

--- Sources/App/Controllers/AuthController.swift
+++ Sources/App/Controllers/AuthController.swift
@@ -6,17 +6,44 @@
 //
 
 import Vapor
+
 class AuthController {
-    func register(_ req: Request) throws -> EventLoopFuture<RegisterResponse> {
-        guard let body = try? req.content.decode(RegisterRequest.self) else {
-            throw Abort(.badRequest)
+    
+    let successResponse = DefaultResponse(
+        result: 1,
+        successMessage: "Запрос успешно обработан.",
+        errorMessage: nil
+    )
+    
+    let errorIncompleteDataResponse = DefaultResponse(
+        result: -2,
+        successMessage: nil,
+        errorMessage: "Неполные данные. Обязательные поля: логин, пароль, адрес эл. почты, имя, фамилия."
+    )
+    
+    let errorWrongCredentials = DefaultResponse(
+        result: -3,
+        successMessage: nil,
+        errorMessage: "Неверный логин или пароль."
+    )
+    
+    let errorInvalidEmail = DefaultResponse(
+        result: -4,
+        successMessage: nil,
+        errorMessage: "Некорректный адрес электронной почты."
+    )
+    
+    func signup(_ req: Request) throws -> EventLoopFuture<DefaultResponse> {
+        guard let body = try? req.content.decode(User.self) else { throw Abort(.badRequest) }
+        
+        guard body.login != nil, body.password != nil, body.name != nil, body.lastname != nil, body.email != nil else {
+            return req.eventLoop.future(errorIncompleteDataResponse)
         }
-        print(body)
-        let response = RegisterResponse(
-            result: 1,
-            user_message: "Регистрация прошла успешно!",
-            error_message: nil
-        )
-        return req.eventLoop.future(response)
+        
+        return req.eventLoop.future(successResponse)
     }
+    
+    
+    
+
 }
