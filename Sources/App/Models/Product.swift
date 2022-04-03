49ec1ac1c548b9c4b83a2e845c8da207c03a6210
Crocodil <evstarshov@icloud.com>
2022-04-03 11:21:01 +0300

    User. Product entities

---
+++ Sources/App/Models/Product.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Евгений Старшов on 03.04.2022.
+//
+
+import Foundation
+
+struct Product: Content {
+    var productId: Int?
+}
---
+++ Sources/App/Models/User.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Евгений Старшов on 03.04.2022.
+//
+
+import Foundation
+
+struct User: Content {
+    let id: Int?
+    let login: String?
+    let password: String?
+    let email: String?
+    let gender: String?
+    let creditCard: String?
+    let bio: String?
+    let name: String?
+    let lastname: String?
+}
